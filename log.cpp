7fc5aa019e049854d8adb0ef33476f5a3eb4f13b
Olafur Waage <olafurw@gmail.com>
2013-07-30 14:38:56 +0000

    Changed logging to use unique_ptr

--- log.cpp
+++ log.cpp
@@ -4,39 +4,38 @@ namespace wot
 {
 namespace log
 {
-    char* current_time()
+    std::unique_ptr<std::string> current_time()
     {
         time_t rawtime;
         tm* timeinfo;
-        char* buffer = new char[80];
+        char buffer[80];
         
         time(&rawtime);
         timeinfo = localtime(&rawtime);
 
         strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
+
+        std::unique_ptr<std::string> result{ new std::string(buffer) };
         
-        return buffer;
+        return result;
     }
 
     void info(const std::string& message)
     {
-        char* ctime = current_time();
-        std::cout << "[INFO] [" << ctime << "] " << message << std::endl << std::flush;
-        delete [] ctime;
+        std::unique_ptr<std::string> ctime = current_time();
+        std::cout << "[INFO] [" << *ctime << "] " << message << std::endl << std::flush;
     }
 
     void warning(const std::string& message)
     {
-        char* ctime = current_time();
-        std::cout << "[WARNING] [" << ctime << "] " << message << std::endl << std::flush;
-        delete [] ctime;
+        std::unique_ptr<std::string> ctime = current_time();
+        std::cout << "[WARNING] [" << *ctime << "] " << message << std::endl << std::flush;
     }
 
     void error(const std::string& message)
     {
-        char* ctime = current_time();
-        std::cout << "[ERROR] [" << ctime << "] " << message << std::endl << std::flush;
-        delete [] ctime;
+        std::unique_ptr<std::string> ctime = current_time();
+        std::cout << "[ERROR] [" << *ctime << "] " << message << std::endl << std::flush;
     }
 }
 }
--- log.hpp
+++ log.hpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
+#include <time.h>
 
 namespace wot
 {
